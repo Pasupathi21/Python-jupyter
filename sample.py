6a03167ff3b8c9483c5d2cfc1727c262722e79e5
Pasupathi-M <pasupathi.muthukumar@kprinfo.com>
2023-08-17 21:45:44 +0530

    DEMO: sample.py program

---
+++ sample.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+{'hellow': '1234', 'world': 2345678}
+
+
+# In[11]:
+
+
+t_dict = {'hellow': '1234', 'world': 2345678}
+print(t_dict.keys())
+
+
+# In[8]:
+
+
+# t_dict.values()
+
+
+# In[12]:
+
+
+# t_dict.pop()
+
